7b8b98e3707cc570dd17bde5fd9e2b7542454265
sudhi <2000sanjaykupanda@gmail.com>
2025-05-01 15:53:54 +0530

    updated version

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['django-greatkart-env.eba-zra4xka2.us-west-2.elasticbeanstalk.com']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
